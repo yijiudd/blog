d132c277e4e5d08be7b566d97002678825487d94
yijiu <yijiubd@gmail.com>
2024-08-13 19:29:20 +0900

    feat:add meta image

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
-import Head from 'next/head'
-import Contact from './components/pages/contact/contact'
-import About from './components/pages/about/about'
-import HeroPage from './components/pages/heroPage/heroPage'
-import Navbar from './components/navbar'
-
-
+import Head from "next/head";
+import Contact from "./components/pages/contact/contact";
+import About from "./components/pages/about/about";
+import HeroPage from "./components/pages/heroPage/heroPage";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta
+          name="image"
+          property="og:image"
+          content="https://blog-yijiudd.vercel.app/person.jpeg"
+        ></meta>
       </Head>
       <div className={styles.container}>
-        <div className={styles.name}><text>Yi Jiu </text></div>
+        <div className={styles.name}>
+          <text>Yi Jiu </text>
+        </div>
         <Navbar></Navbar>
         <HeroPage></HeroPage>
         <About></About>
         <Contact></Contact>
       </div>
     </>
-  )
+  );
 }
